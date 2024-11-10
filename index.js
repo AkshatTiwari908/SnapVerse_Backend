b07caebd67c610ec8bcb30246da25f0ed5b401b8
irudraksh <rudrasoni1414@gmail.com>
2024-11-10 16:11:36 +0530

    maintained with the dev branch

--- index.js
+++ index.js
@@ -1,29 +1,42 @@
-const express = require("express");
-const { Server } = require("socket.io");
+const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const { Server } = require('socket.io');
+const http = require('http');
+const postRoutes = require('./routes/postRoutes');
+const commentRoutes = require('./routes/commentRoutes');
+const Messenger = require('./routes/chats');
+const oneToOnesocket = require('./sockets/o-oSocket');
+const dotenv = require('dotenv');
+const connectDb = require('./db/connectDb.js');
+const authRoutes = require('./routes/auth.route.js');
+const cors = require('cors');
 
-const http = require("http");
-const Messenger = require("./routes/chats");
-const oneToOnesocket = require("./sockets/o-oSocket");
-const dotenv = require("dotenv");
-const  connectDb  = require("./db/connectDb.js");
-const authRoutes = require("./routes/auth.route.js");
 dotenv.config();
-
-const cors = require("cors");
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Middleware
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cors());
-app.use("/api/messages", Messenger);
-app.use("/api/auth", authRoutes);
+app.use('/public', express.static('public'));  
 
-oneToOnesocket(io);
-app.use(express.json());
-const Port = process.env.PORT || 3000;
-app.listen(3000, () => {
-  connectDb();
-  console.log("Server is running on port: ", Port);
+// Routes
+app.use('/posts', postRoutes);  
+app.use('/posts', commentRoutes);  
+app.use('/api/messages', Messenger);  
+app.use('/api/auth', authRoutes);  
+
+// Socket.IO Setup
+oneToOnesocket(io);  
+
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  connectDb();  
+  console.log(`Server is running on port ${PORT}`);
 });
