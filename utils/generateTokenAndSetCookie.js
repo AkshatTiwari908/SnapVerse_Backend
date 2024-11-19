68b1605cc16ce996148416f7dc4dad87e3aa800d
ghost <akshattiwari487@gmail.com>
2024-11-20 02:05:23 +0530

    Token issue

--- utils/generateTokenAndSetCookie.js
+++ utils/generateTokenAndSetCookie.js
@@ -8,7 +8,8 @@ const generateTokenAndSetCookie = (res, userId) => {
 	res.cookie("token", token, {
 		httpOnly: true,
 		/* secure: process.env.NODE_ENV === "production", */
-		sameSite: "strict",
+		/* sameSite: "strict", */
+		secure:true,
 		maxAge: 7 * 24 * 60 * 60 * 1000,
 	});
 
