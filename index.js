e99f6772770a39ee8e0f1fe60cff11fc26257d40
ghost <akshattiwari487@gmail.com>
2024-11-20 14:29:17 +0530

    Cors error fix

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.set("view engine","ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const corsOptions = {
-  origin: '*', 
+  origin: 'http://localhost:5173', 
   methods: ['GET', 'POST', 'PUT', 'DELETE'], 
   allowedHeaders: ['Content-Type', 'Authorization'], 
   credentials: true,  
