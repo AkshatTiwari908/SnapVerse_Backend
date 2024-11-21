14f9b852d26c25e942f202e5e3e96b4cc8bc2515
ghost <akshattiwari487@gmail.com>
2024-11-22 01:57:50 +0530

    Welcome a new route to open any users data

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -1,13 +1,18 @@
 const User = require('../models/users.js');
+const Post = require('../models/post.js')
 const followingsFollowersControllers = require('../controllers/arrayFollowErsIngsControllers.js')
 module.exports.getLoggedInUser = async (req,res)=>{
     const userId =  req.userId 
    try {
        const user = await User.findById(userId);
+       const posts = await Post.find({ user: userId }).populate('user', 'name email').populate('likes', 'name email').populate('comments.user', 'name email');
        if (!user) {
            return res.status(404).json({ error: 'User not found' });
        }
-       res.status(200).json(user);
+       res.status(200).json({
+        user:user,
+        posts:posts
+      });
    } catch (error) {
        res.status(500).json({error:'failed to fetch user data'})
    }
@@ -34,4 +39,20 @@ module.exports.getLoggedInUser = async (req,res)=>{
         console.error('Error fetching followings and followers:', error);
         res.status(500).json({ message: 'Server error. Please try again later.' });
       }
+  }
+  module.exports.getUserProfile = async (req,res)=>{
+    const userId =  req.params.id 
+   try {
+       const user = await User.findById(userId);
+       const posts = await Post.find({ user: userId }).populate('user', 'name email').populate('likes', 'name email').populate('comments.user', 'name email');
+       if (!user) {
+           return res.status(404).json({ error: 'User not found' });
+       }
+       res.status(200).json({
+        user: user,
+        posts: posts
+    });
+   } catch (error) {
+       res.status(500).json({error:'failed to fetch user data'})
+   }
   }
