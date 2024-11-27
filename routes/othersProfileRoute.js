8e6fb8180638e1bb0c13f70d1eed57d7fe660b28
ghost <akshattiwari487@gmail.com>
2024-11-27 22:05:16 +0530

    welcome other user's profile

--- routes/othersProfileRoute.js
+++ routes/othersProfileRoute.js
@@ -2,5 +2,5 @@ const express = require('express')
 const router = express.Router()
 const verifyToken = require('../middleware/middleware.js'); 
 const homePageControllers = require('../controllers/homePageController.js')
-router.get('/user/:id',verifyToken,homePageControllers.getUserProfile)
+router.get('/:id',homePageControllers.getUserProfile)
 module.exports = router
